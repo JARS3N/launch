d3abf799d413418285b32cecc81946d18478510f
JARS3N <JARS3N@users.noreply.github.com>
2017-10-05 12:14:06 -0400

    Update shortcut.R

--- R/shortcut.R
+++ R/shortcut.R
@@ -1,4 +1,4 @@
-shortcut<-(){
+shortcut<-function(){
 R.utils::createWindowsShortcut(
   pathname=file.path(gsub("Documents","Desktop",Sys.getenv()['R_USER']),"seastar.lnk"),
   target= file.path(system.file(package="launch"),"seastar.exe")
