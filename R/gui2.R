5afe88ae493f87c34f84f5731ad1f66afe7a4708
JARS3N <JARS3N@users.noreply.github.com>
2018-01-16 11:40:10 -0500

    Update gui2.R

--- R/gui2.R
+++ R/gui2.R
@@ -7,7 +7,8 @@ gui2<-function(){
     source(str)
   }
   onExit<-function(){
-   Exit<<-TRUE
+   Exit<-TRUE
+    return(NULL)
   }
   win1 <- tktoplevel()
   tktitle(win1)<-"Seastar"
@@ -21,7 +22,7 @@ gui2<-function(){
     tkinsert(win1$env$lst, "end", fruit)
 
   tkselection.set(win1$env$lst, 10)
-  tkgrid(win1$env$butOK,win1$env$exits)
+  tkgrid(win1$env$butOK)
   while(EXIT==FALSE){
   }
 }
