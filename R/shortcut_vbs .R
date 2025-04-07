9899bfd829c4475fecc4f6f1639ea76823fb74d3
JARS3N <JARS3N@users.noreply.github.com>
2025-04-07 16:03:59 -0400

    Update shortcut_vbs .R

--- R/shortcut_vbs .R
+++ R/shortcut_vbs .R
@@ -1,4 +1,4 @@
-shortcut_vbs <-function(ScriptName){
-  LNKcreateVBS <- system.file(package="launch",path="vbs/shortcut.vbs")
-  shell(paste("Cscript //B ",shQuote(LNKcreateVBS),shQuote(ScriptName),sep=" ")) 
+shortcut_vbs <- function(ScriptName, folder) {
+  LNKcreateVBS <- system.file(package = "launch", path = "vbs/shortcut.vbs")
+  shell(paste("Cscript //nologo //B", shQuote(LNKcreateVBS), shQuote(ScriptName), shQuote(folder)))
 }
